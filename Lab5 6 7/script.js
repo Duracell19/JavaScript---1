a6ae60633ed47062569ed0ebe0f5d013684e0560
Alexandra <sashutkabrest@mail.ru>
2017-01-03 00:25:05 +0300

    Lab 5 6 7 was overwritten!

--- Lab5 6 7/script.js
+++ Lab5 6 7/script.js
@@ -1,30 +1,22 @@
 var app = angular.module('app', []);
-
 app.controller('firstController', function ($http, $scope, factor){
 	$http.get('https://api.nytimes.com/svc/books/v3/lists.json?api-key=8fe7ff4cab9e4af09fc034cfeafa7f2f&list=e-book-fiction')
 		.success(function(result){
 			$scope.results = result.results;
-		})
-		.error(function(data, status){
-			console.log(data);
-			console.log(status);
+			});
+			$scope.factor = factor;
 		});
-	$scope.filt;
-	$scope.postInf = {};
-	$scope.factor=factor;
-});
-
 app.controller('fromForm', function ($scope, factor){
-  	$scope.postInf = {};
-  	$scope.factor=factor;
-  	$scope.save = function(post) {
-		angular.copy(post, factor.postInf);
-	}
-});
-
-app.factory('factor',function(){
-	return {
-		postInf: {
-		}
-	}
-});
+  	$scope.save = function(title, link, author, publisher) {
+			$scope.info = [];
+			$scope.info.push({
+					title: title,
+					link: link,
+					author: author,
+					publisher: publisher
+				});
+				angular.copy($scope.info[0], factor.information);
+			}
+		});
+		app.factory('factor', function() { return { information: {} }
+	});
